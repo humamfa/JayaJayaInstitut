eedeafaf325ad0e80f5e85582a2f5aef48788583
Humam Fawwaz Abiyyu Binekas <152384891+humamfa@users.noreply.github.com>
2024-06-16 01:01:32 +0700

    Update students_performance_app.py

--- students_performance_app.py
+++ students_performance_app.py
@@ -6,7 +6,7 @@ from prediction import prediction
 
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("https://static.vecteezy.com/system/resources/thumbnails/012/377/764/small/graduation-line-icon-png.png", width=55)
+    st.image("https://png.pngtree.com/png-vector/20211120/ourmid/pngtree-university-logo-png-image_4037518.png", width=55)
 with col2:
     st.header('Students Performance App (Prototype)')
 
